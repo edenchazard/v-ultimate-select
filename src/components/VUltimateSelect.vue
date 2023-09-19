22da4f187ee42e0c5fac4f931d111882eeae3c30
Charley Reeson <neon.fortytwo@gmail.com>
2023-09-19 23:25:33 +0100

    wake up babe new docs have arrived

--- src/components/VUltimateSelect.vue
+++ src/components/VUltimateSelect.vue
@@ -130,80 +130,190 @@ import { useElementSize, useResizeObserver } from "@vueuse/core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 const props = defineProps({
+  /**
+   * The id of the selected option's value.
+   *
+   * If `multiple` is true, this will be an array of the option IDs.
+   */
   ids: {
     type: [String, Array<string>],
     default: null,
   },
 
+  /**
+   * TODO
+   *
+   * wanna make this a v-model of all the selected values
+   */
   values: {
     type: Object as PropType<OptionValue[] | OptionValue>,
     default: () => {},
   },
 
+  /**
+   * The options list.
+   *
+   * A string array or an array of objects can be passed. If passing
+   * an object, make sure to specify the props `labelField` and `trackByKey`
+   */
   options: {
     type: Object as PropType<String[] | Record<string, any>[]>,
     required: true,
   },
+
+  /**
+   * Allow the user to select multiple options.
+   *
+   * Works in tandem with `minimumSelections` and `maximumSelections`
+   */
   multiple: {
     type: Boolean,
     default: false,
   },
+
+  /**
+   * Close the dropdown when an option has been selected.
+   *
+   * This property has no effect if `listbox` is true.
+   */
   closeOnSelect: {
     type: Boolean,
     default: true,
   },
+
+  /**
+   * TODO
+   * If no option has been picked, the placeholder text will appear.
+   */
   placeholder: {
     type: String,
     default: "Select...",
   },
 
+  /**
+   * A unique key for each option that differentiates it from other options
+   * in the list.
+   */
   trackByKey: {
     type: [String, Number] as PropType<OptionKey>,
     default: "value",
   },
+
+  /**
+   * A key to use for displaying an option's label. This should be the
+   * text you'd like to present the user with.
+   */
   labelField: {
     type: String,
     default: "value",
   },
 
-  minimumOptions: {
+  /**
+   * TODO
+   * The minimum number of options a user must select.
+   */
+  minimumSelections: {
     type: Number,
-    default: 1,
+    default: 0,
   },
+
+  /**
+   * TODO
+   * The maximum number of options a user must select.
+   *
+   * To not restrict the maximum number of options selected, leave this
+   * value null.
+   */
+  maximumSelections: {
+    type: Number,
+    default: null,
+  },
+
+  /**
+   * The max height in pixels for the dropdown.
+   */
   dropDownMaxHeight: {
     type: Number,
     default: 300,
   },
+
+  /**
+   * TODO
+   */
   fullPageIfDropdownTooBig: {
     type: Boolean,
     default: true,
   },
+
   /**
+   * A html id attribute to give the component.
+   *
    * If an id isn't specified, one will be generated automatically.
    */
   htmlId: {
     type: String as PropType<HTMLAttributes["id"]>,
     required: false,
   },
+
+  /**
+   * Show the search box. You can specify how the search is performed
+   * with the `searchHandler` prop.
+   */
   showSearch: {
     type: Boolean,
     default: true,
   },
+
+  /**
+   * Display the options list as a listbox instead of a dropdown.
+   */
   listbox: {
     type: Boolean,
     default: false,
   },
-  /*   searchHandler: {
+
+  /**
+   * TODO
+   *
+   * A callback that defines how the combobox will filter options
+   * when searching.
+   */
+  searchHandler: {
     type: Object as PropType<(option: Option) => boolean>,
-    default: () => true,
-  }, */
+    default: null,
+  },
 });
 
 const emit = defineEmits<{
+  /**
+   * Emitted whenever the combobox is opened.
+   *
+   * This event doesn't trigger if `listbox` is true.
+   */
   (event: "open"): void;
+
+  /**
+   * Emitted whenever the combobox is closed.
+   *
+   * This event doesn't trigger if `listbox` is true.
+   */
   (event: "close"): void;
+
+  /**
+   * Emitted whenever an option has been selected.
+   */
   (event: "selected", key: OptionKey, value: OptionValue): void;
+
+  /**
+   * Emitted whenever the combobox's value has been changed.
+   */
   (event: "update:ids", value: SelectValue): void;
+
+  /**
+   * TODO
+   *
+   * Emitted whenever the combobox's value has been changed.
+   */
   (event: "update:values", values: OptionValue | OptionValue[]): void;
 }>();
 
